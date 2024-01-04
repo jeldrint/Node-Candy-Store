2afe3d78f01bd9880b65845c96bef9c91d647e30
John Eldrin Tolentino <115896724+jeldrint@users.noreply.github.com>
2024-01-04 22:34:46 +0800

    minor update

--- populatedb.js
+++ populatedb.js
@@ -1,40 +1,55 @@
 #! /usr/bin/env node
 
 console.log(
-    'This script populates some test books, authors, genres and bookinstances to your database. Specified database as argument - e.g.: node populatedb "mongodb+srv://cooluser:coolpassword@cluster0.lz91hw2.mongodb.net/local_library?retryWrites=true&w=majority"'
+    'This script populates. Specified database as argument - e.g.: node populatedb "mongodb+srv://cooluser:coolpassword@cluster0.lz91hw2.mongodb.net/local_library?retryWrites=true&w=majority"'
   );
   
-  // Get arguments passed on command line
-  const userArgs = process.argv.slice(2);
-  
-  const Category = require('./models/category');
-  const Items = require('./models/items');
+// Get arguments passed on command line
+const userArgs = process.argv.slice(2);
+
+const Category = require('./models/category');
+const Items = require('./models/items');
 
 
-  const categories = [];
-  const items = [];
+const categories = [];
+const items = [];
 
-  const mongoose = require('mongoose');
-  mongoose.set('strictQuery', false);
+const mongoose = require('mongoose');
+mongoose.set('strictQuery', false);
 
-  const mongoDB = userArgs[0];
+const mongoDB = userArgs[0];
 
-  main().catch((err) => console.log(err));
+main().catch((err) => console.log(err));
 
-  async function main() {
-    console.log("Debug: About to connect");
-    await mongoose.connect(mongoDB);
-    console.log('Debug: Should be connected?')
-    await createCategories();
-    await createItems();
-    console.log('Debug: Closing mongoose');
-    mongoose.connection.close();
-  }
+async function main() {
+  console.log("Debug: About to connect");
+  await mongoose.connect(mongoDB);
+  console.log('Debug: Should be connected?')
+  await createCategories();
+  //await createItems();
+  console.log('Debug: Closing mongoose');
+  mongoose.connection.close();
+}
 
 //  importing created dbs
+//  creating categories
 async function categoryCreate(index, name) {
   const category = new Category({ name:name });
   await category.save();
   categories[index] = category;
   console.log(`Added Category: ${name}`)
+}
+
+async function createCategories() {
+  console.log('creating categories');
+  await Promise.all([
+    categoryCreate(0, "Hard Candy"),
+    categoryCreate(1, "Sour Candy"),
+    categoryCreate(2, "Cotton Candy"),
+    categoryCreate(3, "Lollipops"),
+    categoryCreate(4, "Gummies"),
+    categoryCreate(5, "Candy Caramels"),
+    categoryCreate(6, "Chocolate"),
+    categoryCreate(7, "Soft Candy"),
+  ])
 }
