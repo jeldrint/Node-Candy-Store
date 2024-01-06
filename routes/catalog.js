e7df0b7e4a303bdfe098cb1139763e82666c7b46
John Eldrin Tolentino <115896724+jeldrint@users.noreply.github.com>
2024-01-06 22:20:49 +0800

    minor changes

--- routes/catalog.js
+++ routes/catalog.js
@@ -1,3 +1,12 @@
 const express = require('express');
 const router = express.Router();
 
+const category_controller = require('../controllers/categoryController')
+const item_controller = require('../controllers/itemController')
+
+
+// CANDY ROUTES //
+router.get('/', category_controller)
+
+
+module.exports = router;
